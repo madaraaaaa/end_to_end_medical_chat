efa2c76846e5ff384c7f69aaec02314402012d17
Mahmoud Afifi <mahmoudsoudi31@gmail.com>
2026-05-05 12:14:21 +0200

    add the files required for the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Mahmoud ",
+    author_email="",
+    packages= find_packages(),
+    intstall_requires=[]
+)
